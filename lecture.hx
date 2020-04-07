bc4cb71770523d7295f58f24f6bd846b520ae3cb
Rebecca Kukshtel <rebecca@betterment.com>
2020-04-06 21:49:00 -0400

    initial commit

---
+++ lecture.hx
@@ -0,0 +1,16 @@
+// Notes from 2018 Talk: https://www.youtube.com/watch?v=CswB_1rSOd0&t=6330s
+
+// Getting Started
+
+// How do you do 2D?
+// What's the main loop across platforms?
+// Runtime asset loading
+// Simple collisions
+// Capabilities of Heaps
+
+// 2D particles system
+
+// Not really:
+// Textures atlases support
+// Performance tuning (memory & cpu profiling)
+// Custom Rendering Setup
